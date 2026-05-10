7e10553fd60e161910fc71198373ee68c273ab1f
Bharath Dasaraju <bhrth.dsra1@gmail.com>
2026-05-10 11:04:19 +0800

    :tada: petclinic spring boot microservices :tada:

---
+++ links.sh
@@ -0,0 +1,16 @@
+https://github.com/spring-petclinic/spring-petclinic-microservices
+
+
+spring-petclinic-microservices
+1. SourceCode - 8 spring boot microservices with REST APIs
+2. Dockerfiles - Multistage docker builds
+3. App Configurations - DB Schemas etc
+4. Tests 
+
+
+Infrastructure:
+1. Terraform Modules  - VPC, EKS, RDS, ECR, Secrets
+2. Helm Charts
+3. ArgoCD + CI Pipelines 
+
+
